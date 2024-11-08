4ff704074cea611a3d7535a415755d4df2056a5a
Swag-blips <coderblip@gmail.com>
2024-11-08 13:25:07 +0100

    feat:refactor tests

--- src/tests/shortUrl.test.ts
+++ src/tests/shortUrl.test.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import { app } from "../server";
 import shortUrl from "../models/shortUrl.model";
 
+jest.setTimeout(60000);
 beforeEach(async () => {
   await mongoose.connect(process.env.MONGO_URI as string);
 });
@@ -17,9 +18,20 @@ describe("POST /api/url", () => {
     expect(res.body.newUrl).toHaveProperty("_id");
     expect(res.body.newUrl.destination).toBe("https://devlinks.space");
   });
+
+  it("returns status code 404 if the destination is not passed", async () => {
+    const res = await request(app).post("/api/url").send({});
+    expect(res.statusCode).toEqual(400);
+  });
 });
+
+
+
 
 afterEach(async () => {
   await shortUrl.deleteMany({});
+});
+
+afterAll(async () => {
   await mongoose.connection.close();
 });
